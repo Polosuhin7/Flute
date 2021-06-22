eba4f59e700b0d4f39145971e5b714d8d3d79ab7
vasilijpolosuhin <polosuhin7@gmail.com>
2021-06-23 00:13:14 +0300

    init

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '06b07edd8387964017e5a55dde84223a'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '862241f8-27be-4ab7-89e3-254b360dfcb5'
+};
