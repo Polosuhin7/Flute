ea59cdeff4f1f2bfa2d2f9426e9c7bf52111cedd
vasilijpolosuhin <polosuhin7@gmail.com>
2021-07-06 16:07:20 +0300

    'wip'

--- backend/public/web-build/service-worker.js
+++ backend/public/web-build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.062286d01425ac17be0312a922263fe8.js"
+  "./precache-manifest.a003a0a69a5286bfbd06a4e5dbb9bad8.js"
 );
 
 workbox.skipWaiting();
