11a650a2010b6eb45b1f08ebd374558e155583f6
vasilijpolosuhin <polosuhin7@gmail.com>
2021-07-08 19:36:01 +0300

    'wip'

--- backend/middlewares/serveStatic/index.js
+++ backend/middlewares/serveStatic/index.js
@@ -14,7 +14,7 @@ module.exports = strapi => {
         const {maxAge, path: publicPath} = strapi.config.middleware.settings.public;
         const staticDir = path.resolve(strapi.dir, publicPath || strapi.config.paths.static);
         const webDir = path.resolve(strapi.dir, publicPath || strapi.config.paths.static, 'web');
-        // strapi.app.use(serve(staticDir));
+        strapi.app.use(serve(staticDir));
         strapi.app.use(serve(webDir));
     }
   };
