0688edfe40b64498f23db2ca058f07253c256b1b
vasilijpolosuhin <polosuhin7@gmail.com>
2021-07-08 21:48:33 +0300

    'wip'

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,6 +1,9 @@
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => {
+  console.log('env', env)
+  return {
     host: env('HOST', '0.0.0.0'),
-    port: env('PORT', 80),
+    port: env('$PORT'),
     url: env('https://flute-service.herokuapp.com/'),
-  });
+  }
+};
    
