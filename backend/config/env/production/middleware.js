cf80e93716719419784a35474c8739c897080de8
vasilijpolosuhin <polosuhin7@gmail.com>
2021-07-08 20:02:27 +0300

    'wip'

--- backend/config/env/production/middleware.js
+++ backend/config/env/production/middleware.js
@@ -2,7 +2,7 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      origin: ['https://flute-service.herokuapp.com', 'https://flute-mobile.herokuapp.com'],
+      origin: ['https://flutes.herokuapp.com', 'https://flute-service.herokuapp.com', 'https://flute-mobile.herokuapp.com'],
     },
     serveStatic: {
       enabled: true
--- backend/config/middleware.js
+++ backend/config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
     cors: {
-      origin: ['http://localhost:1337', 'http://192.168.0.104:19006', 'http://localhost:19006', 'https://example.com', 'https://www.example.com'],
+      origin: ['http://localhost:1337', 'http://192.168.0.104:19006', 'http://localhost:19006', 'https://flutes.herokuapp.com'],
     },
     serveStatic: {
       enabled: true
