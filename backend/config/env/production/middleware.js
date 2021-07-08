cbde502fe512db10bc3f499190771acb719eca70
vasilijpolosuhin <polosuhin7@gmail.com>
2021-07-08 16:25:53 +0300

    'wip'

--- backend/config/env/production/middleware.js
+++ backend/config/env/production/middleware.js
@@ -2,7 +2,7 @@ module.exports = {
   settings: {
       cors: {
         enabled: true,
-        origin: ['https://flute-service.herokuapp.com'],
+        origin: ['https://flute-service.herokuapp.com', 'https://flute-mobile.herokuapp.com'],
       }, 
   }
 };
